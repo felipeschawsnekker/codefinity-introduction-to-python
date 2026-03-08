86d2d1e6e5dee84c15f248e76c60ee5732bc40d2
felipeschawsnekker <>
2026-03-08 19:18:27 +0000

    Challenge: Automating Stock Control

--- loops/challenge_automating_stock_control/main.py
+++ loops/challenge_automating_stock_control/main.py
@@ -1,4 +1,5 @@
 # Initialize the inventory dictionary with stock details
+discount_threshold = 100
 inventory = {
     "Bread": [30, 50, 10, False],   # "Item": [current stock, minimum stock, restock quantity, on sale (True/False)]
     "Eggs": [120, 200, 40, False],
@@ -6,4 +7,49 @@ inventory = {
     "Apples": [15, 50, 15, False]
 }
 
-discount_threshold = 100
+for item_name in inventory:
+    #Item": [current stock, minimum stock, 
+    #restock quantity, on sale (True/False)]
+    current_stock = inventory[item_name][0]
+    minimum_required_stock=inventory[item_name][1]
+    restock_quantity=inventory[item_name][2]
+    sale_status=inventory[item_name][3]
+    necessary_to_restock=minimum_required_stock-current_stock
+
+    #RESTOCKING
+    while current_stock<minimum_required_stock:
+        current_stock+=restock_quantity
+        inventory.update()
+        if current_stock>=minimum_required_stock:
+            if(restock_quantity-necessary_to_restock>0):
+                restock_quantity-=necessary_to_restock
+            else:
+                restock_quantity=0
+            print(f"Processing {item_name}")
+            
+    inventory[item_name][0]=current_stock
+    inventory[item_name][1]=minimum_required_stock
+    inventory[item_name][2]=restock_quantity
+    inventory[item_name][3]=sale_status
+    if(current_stock>discount_threshold and inventory[item_name][3]==False):
+        inventory[item_name][3]=True
+        
+        
+print("Processing completed")
+    
+print(inventory)
+
+
+ 
+
+    
+"""
+print("Current stock: ",current_stock)
+print("Minimum required stock",minimum_required_stock)
+print("Restock quantiy",restock_quantity)
+print("Sale status: ",sale_status)
+print(" ")*#
+"""
+    
+
+
